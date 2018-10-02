6dd0796ba3084c0c54d09b660541072357f4f9d9
Jose Developer <jmfss1@iscte-iul.pt>
2018-10-02 23:34:40 +0100

    Segunda tentativa

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < 10; i++) {
+			Num n = new Num(i);
 			System.out.println("Hello World");
+		}
 	}
 
 }
